41b682c6679ed6777cc361240b8508010e5ff2a6
genechen466 <genechen1106@gmail.com>
2026-07-16 17:35:55 +0000

    change undeploy order (from UE to CN)

--- script/undeploy.sh
+++ script/undeploy.sh
@@ -16,6 +16,17 @@ require_commands kubectl helm
 info "Cleaning 5GMAP OAI RAN deployment"
 
 slice_end=$((NUM_SLICES + 9))
+total=$((NUM_USERS * NUM_SLICES))
+
+for ((offset=0; offset<total; offset++)); do
+    u=$((10 + offset))
+    kubectl delete deployment "oai-dnn$u" -n "$NAMESPACE" --ignore-not-found=true
+    helm_uninstall_if_exists "nrue$u" "$NAMESPACE"
+    helm_uninstall_if_exists "gnbdu$u" "$NAMESPACE"
+    helm_uninstall_if_exists "gnbcu$u" "$NAMESPACE"
+    helm_uninstall_if_exists "gnb$u" "$NAMESPACE"
+done
+
 for ((s=10; s<=slice_end; s++)); do
     helm_uninstall_if_exists "nrf$s" "$NAMESPACE"
     helm_uninstall_if_exists "udr$s" "$NAMESPACE"
@@ -24,16 +35,6 @@ for ((s=10; s<=slice_end; s++)); do
     helm_uninstall_if_exists "amf$s" "$NAMESPACE"
     helm_uninstall_if_exists "smf$s" "$NAMESPACE"
     helm_uninstall_if_exists "upf$s" "$NAMESPACE"
-done
-
-total=$((NUM_USERS * NUM_SLICES))
-for ((offset=0; offset<total; offset++)); do
-    u=$((10 + offset))
-    helm_uninstall_if_exists "gnb$u" "$NAMESPACE"
-    helm_uninstall_if_exists "gnbcu$u" "$NAMESPACE"
-    helm_uninstall_if_exists "gnbdu$u" "$NAMESPACE"
-    helm_uninstall_if_exists "nrue$u" "$NAMESPACE"
-    kubectl delete deployment "oai-dnn$u" -n "$NAMESPACE" --ignore-not-found=true
 done
 
 if [ "$DELETE_MYSQL" = "1" ]; then
