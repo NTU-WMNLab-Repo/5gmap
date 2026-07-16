541478af8a8b1afe52d9e84ab547253aa0d91de1
genechen466 <genechen1106@gmail.com>
2026-07-16 15:09:09 +0000

    fix ran unknown option --sa, add gNB_DU_ID

--- script/deploy.sh
+++ script/deploy.sh
@@ -178,6 +178,7 @@ deploy_oai_ran_user() {
     set_yaml_value "$cu_chart/values.yaml" gnbcuName "\"oai-gnb-cu$u-rfsim\""
     set_yaml_value "$cu_chart/values.yaml" f1cuIpAddress "\"status.podIP\""
     set_yaml_value "$cu_chart/values.yaml" f1duIpAddress "\"oai-gnb-du$u\""
+    set_yaml_value "$cu_chart/values.yaml" useAdditionalOptions "\"--rfsim --log_config.global_log_options level,nocolor,time\""
     sed -i "/nodeSelector:/,/nodeName:/c\nodeSelector:\n  deplocation: $RAN_LOC\n\nnodeName: " "$cu_chart/values.yaml"
     helm upgrade --install "gnbcu$u" "$cu_chart" -n "$NAMESPACE"
     wait_for_pod "$NAMESPACE" "oai-gnb-cu$u" 420 45
@@ -196,6 +197,7 @@ deploy_oai_ran_user() {
     set_yaml_value "$du_chart/values.yaml" gnbduName "\"oai-gnb-du$u-rfsim\""
     set_yaml_value "$du_chart/values.yaml" f1cuIpAddress "\"oai-gnb-cu$u\""
     set_yaml_value "$du_chart/values.yaml" f1duIpAddress "\"status.podIP\""
+    set_yaml_value "$du_chart/values.yaml" useAdditionalOptions "\"--rfsim --log_config.global_log_options level,nocolor,time\""
     sed -i "/nodeSelector:/,/nodeName:/c\nodeSelector:\n  deplocation: $RAN_LOC\n\nnodeName: " "$du_chart/values.yaml"
     helm upgrade --install "gnbdu$u" "$du_chart" -n "$NAMESPACE"
     wait_for_pod "$NAMESPACE" "oai-gnb-du$u" 420 45
